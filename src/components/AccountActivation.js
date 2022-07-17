05f8351a7c638d2e0b6c11d2aa08f4e0ee55bbea
Tuğçe Türkmenler <99015069+turkmenlertugce@users.noreply.github.com>
2022-07-17 13:46:43 +0300

    Update AccountActivation.js

--- src/components/AccountActivation.js
+++ src/components/AccountActivation.js
@@ -1,9 +1,16 @@
+
+
+
+
+
+
 import axios from 'axios'
 import React, { useEffect, useState, useCallback } from 'react'
 import { useNavigate,useLocation } from 'react-router-dom';
 
 const AccountActivation = () => {
-
+    const [password,setPassword] = useState("")
+    const [passwordAgain,setPasswordAgain] = useState("")
     const location = useLocation();
    
 
@@ -16,7 +23,7 @@ const AccountActivation = () => {
     }, [])
 
     const activate = useCallback(async () => {
-        axios.post(`https://faxriboot-env.eba-dincnkef.us-east-1.elasticbeanstalk.com/api/activate_account/confirm?token=${location.pathname.substring(19)}`).
+        axios.post(`https://faxriboot-env.eba-dincnkef.us-east-1.elasticbeanstalk.com/api/activate_account/confirm?token=${location.pathname.substring(19)}`, {password:password,passwordAgain:passwordAgain}).
         then(res => {
             console.log(res.data);
             navigation("/");
@@ -46,10 +53,17 @@ const AccountActivation = () => {
   return (
     <div>
         <h3>Account Activation</h3>
-        <div>
-                <h2>Activated</h2>
-                <p></p>
-            </div>
+        <div className="form-group">
+            <label>Password</label>
+            <input type="password" className="form-control" placeholder="Password" value={password}
+            onChange={e => setPassword(e.target.value)}/>
+        </div>
+        <div className="form-group">
+            <label>Password Again</label>
+            <input type="password" className="form-control" placeholder="Password" value={passwordAgain}
+            onChange={e => setPasswordAgain(e.target.value)}/>
+        </div>
+        <button onClick={activate} className="btn.btn-primary btn-block">Activate</button>
     </div>
   )
 }
